afcbb1c1d5b7b1d8e82f62d6bd9f418a2c03feaa
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2019-10-08 18:01:38 +0100

    Update Tutorial1.py

--- Tutorial1.py
+++ Tutorial1.py
@@ -18,4 +18,16 @@ just singing in the rain"""
 print(message)
 #strings NOT in this format will NOT work over multiple lines
 
-#
+#function len() , short for length, returns the length of the string, starting at 1
+message = "abcdefg"
+print(len(message))
+
+#Strings are jsut strings of characters, and you can request specific characters using [].
+message = "abcdefg"
+print(message[0])
+#Keep in mind that the first character is stored at 0, second at 1, ETC.
+
+#calling a negative number inside [] prints the digit that number from the end, starting a -1
+message = "abcdefg"
+print(message[-1])
+
