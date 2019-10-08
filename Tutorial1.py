53b4a42819d2831a44878a9d9b134878ec3df638
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2019-10-08 17:39:52 +0100

    Update Tutorial1.py
    Tutorial documentation on using strings.

---
+++ Tutorial1.py
@@ -0,0 +1,21 @@
+#Strings
+
+#single quotes
+message = 'Hello World'
+print(message)
+
+#using double quotes to avoid error from apostrophe.
+message = "Bobby's World"           
+print(message)
+
+#using an escape(\) to handle apostrophe
+message = 'Bobby\'s World'
+print(message)
+
+#triplestrings/docstrings(Strings in three sets of quotes) hold true over multiple lines
+message = """I'm singing in the rain
+just singing in the rain"""
+print(message)
+#strings NOT in this format will NOT work over multiple lines
+
+#
