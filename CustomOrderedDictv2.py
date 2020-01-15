0b7deff1878fecd97b6cf27d5b454127d6de0bed
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-01-15 23:38:49 +0000

    Improved CustomOrderedDict.py
    Modified to allow for potential expansion using loops. Output is formatted differently. Otherwise, not much different.

---
+++ CustomOrderedDictv2.py
@@ -0,0 +1,21 @@
+
+
+with open("names_redo.csv", "r") as input:
+    def oDict(rFile):
+        #keys = rFile.readline()     
+        keys = (rFile.readline().strip("\n").split(","))
+        #Get keys from first line
+        dicts = []                  #Generate an empty dictionary to store the dicts in
+
+        print(keys)
+
+        for i in rFile.readlines():
+            dicts.append((keys[0], i.strip("\n").split(",")[0]))
+            dicts.append((keys[1], i.strip("\n").split(",")[1]))
+            dicts.append((keys[2], i.strip("\n").split(",")[2]))
+        
+        print(dicts)
+        #print(dicts[0])
+
+    oDict(input)
+
