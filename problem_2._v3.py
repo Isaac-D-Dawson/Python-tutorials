6be5ad1009120341b9dcbcfb08040bdff87d55b3
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-01-15 23:51:59 +0000

    Created files
    Base document and time-mesuring code by Chucks Orefo. Solution assembled by myself. V2 included minor logic changes to facilitate replacement with a function, v3 modified to handle arbitrarily long lists with functions and looping.

---
+++ problem_2._v3.py
@@ -0,0 +1,88 @@
+from bank import database,time_convert
+import time
+
+##################################################################
+#                         Your Mission                           #
+##################################################################
+
+# 1) identify the number of accounts in the database
+# 2) access each account and withdraw £5............
+# 3) update the database with new value after you have within drawn your desired amount....
+# 4) add the withdrawn amount to the spoofed_account
+
+#thing to keep in mind
+#you will need to assign the imported database to data_
+#gather some intel from Bank on how to do this....
+
+data_ = database()
+spoofed_account = [] #this means an empty list 
+
+# to figure out the number of accounts in the bank you may want to loop over the database
+# using a 'for loop'
+
+# #for loop use case example:
+# greetings = ['hello','goodday','howdy','yo','sup']
+
+# for greeting in greetings: # take note of the ':'
+#     print(greeting) # and also the indentation!!
+
+# # the for loop is a very powerful tool that may save you time....
+# # other tools you may need:
+# #type() 
+# #str()
+# #int()
+
+# # .append() use case example:
+# print(f'Account Before :{spoofed_account}')
+# spoofed_account.append(f'£{100}')
+# print(f'Account After :{spoofed_account}')
+
+# # # .strip('?') use case example:
+# # var = 'A string'
+# # print(f'Before: {var}')
+# # var = var.strip('A') #remember strings are immutable 
+# # print(f'After: {var}')
+
+
+
+
+
+########################################################
+#                 END OF INSTRUCTIONS                  #
+########################################################
+
+#what the accounts look like before your hack
+print(f'Before: {data_}')
+
+########################################################
+#your code goes below !!
+t0 = time.time()
+########################################################
+
+#data_ = database()
+spoofed_account = ["Avery T D Harry", "£0"]
+
+#print(help(int()))
+targets = data_.index(data_[-1])
+print(f"number of accounts targeted:{targets+1}")
+
+def transfer(targ_account, amount):
+    temp = targ_account[1].strip("£")
+    targ_account[1] = (f"£{int(temp)+amount}")
+
+for i in data_:
+    transfer(i, -5)
+    transfer(spoofed_account, +5)
+
+#########################################################
+t1 = time.time()
+#########################################################
+
+#what the accounts look like after your hack
+print(f'After: {data_}')
+
+#########################################################
+
+t = t1-t0
+print(f'your hack took {time_convert(t)}')
+
