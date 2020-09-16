f6abe1bda6c2aef304e1c0d070b1fa3174f1944f
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-09-16 13:25:39 +0100

    Created ListFilesInDir.py
    An exceedingly simple program that lists all files and folders in a given directory where it is run. I...I have no idea why I was asked to make this.

---
+++ ListFilesInDir.py
@@ -0,0 +1,11 @@
+#ListFilesInDir.py      04.02.2020
+
+#program #8
+
+#Get the OS module
+import os
+
+#Prent everything in the current working directory
+print(os.listdir())
+
+#I...I'm not sure what the goal was here.
