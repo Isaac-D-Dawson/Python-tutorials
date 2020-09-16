c985b0ebe45f306c23840af3f184b71071891b1e
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-09-16 14:03:45 +0100

    Created nlengthConcatenator.py
    Some example code that takes a list of strings and return s them concatenated into a single string.

---
+++ nlengthConcatenator.py
@@ -0,0 +1,15 @@
+#NlengthConcatenator.py     04.02.2020
+
+#program #12
+
+#make a lsit to store our strings in
+strings = ["Test", " is", " Not", " the", " Future.", "Or ", "The ", "Past..."]
+#define an output
+outString = ""
+
+#iterate over it
+for i in strings:
+    #and append them
+    outString= outString + i
+
+print(outString)
