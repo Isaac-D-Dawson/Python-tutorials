c8167e5fc3f9af9faaf5130bcfe02a3c99d9e8d8
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-09-16 13:29:44 +0100

    Created MoreTha17.py
    A simple piece of code that takes in a number and returns it. if the number is more than 17, it has 17 subtracted from it and is doubled. else it is jsut subtracted from 17.

---
+++ MoreThanSeventeen.py
@@ -0,0 +1,14 @@
+#MoreThan17?.py     03.02.2020
+
+#program #5
+
+print("Enter number:")
+ourNumber = int(input())
+
+#is it more than 17?
+if ourNumber > 17:
+    #subtract it, double it.
+    print((ourNumber-17)*2)
+else:
+    #just subtract it and be done with it.
+    print(17-ourNumber)
