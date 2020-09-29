63e597d0f39cefd343ea99dc1bb9d7b12d9164c7
Isaac Dawson <isaacdawson1837@gmail.com>
2020-09-29 16:50:29 +0100

    Upladed TwoLists.py, a file that prints a string of numbered objects from a list. Created based off a programming excerise, I'm uploading it purely for archival purposes.

---
+++ TwoLists.py
@@ -0,0 +1,15 @@
+#TwoLists.py        04.02.2020
+
+#program 12
+
+#create our list:
+creatures = ["Snake","Rabbit","Dog","Poney","Lobster","Tiger"]
+
+#for loop one
+for i in enumerate(creatures):
+    print(i)
+
+#for loop two:
+
+for index, i in enumerate(creatures):
+    print(f"{index} {i}")
